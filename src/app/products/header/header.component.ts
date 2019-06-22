f3577d51711c0ec73fbea58276808cf3f4bb101a
Narek Grigoryan <gnaro93@gmail.com>
2019-06-22 14:11:35 +0400

    fix-data

--- src/app/products/header/header.component.ts
+++ src/app/products/header/header.component.ts
@@ -9,6 +9,44 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  fluid: Array<object> = [
+    {
+      title: 'Описание продукта',
+      // content: '',
+      icon: 'assets/icons/file.png',
+      next: 'assets/icons/arrow-right.png',
+      width: '100%',
+      color: 'purple'
+    },
+    {
+      title: 'Скачать',
+      content: 'Докум-ция, программы и драйверы',
+      icon: 'assets/icons/path.png',
+      // next: 'assets/icons/arrow-right.png',
+      width: '66.6666666667%',
+      color: 'green',
+      wrap: 'left'
+    },
+    {
+      title: 'Примеры',
+      content: 'C# API',
+      icon: 'assets/icons/grid.png',
+      // next: 'assets/icons/arrow-right.png',
+      width: '66.6666666667%',
+      color: 'blue',
+      wrap: 'left'
+    },
+    {
+      title: 'Видео',
+      content: 'Смотреть',
+      icon: 'assets/icons/video.png',
+      next: 'assets/icons/arrow-right.png',
+      width: '33.3333333333%',
+      color: 'yellow',
+      mTop: '-18.2%'
+    },
+  ];
+
   ngOnInit() {
   }
 
